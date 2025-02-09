00ec8e3c893aa45c8e81b5c1af0eff4a85fac4b0
sphinx91889 <mevin.riviere@gmail.com>
2025-02-09 18:05:04 -0500

    Update BestSpaSection.jsx

--- src/components/BestSpaSection.jsx
+++ src/components/BestSpaSection.jsx
@@ -1,18 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react';
 
 const BestSpaSection = () => {
-  useEffect(() => {
-    // Dynamically load the external script for map authorization.
-    const script = document.createElement('script')
-    script.src = "https://embedmaps.com/google-maps-authorization/script.js?id=327d86f0388a494678b41ab0d16e308d2505298c"
-    script.async = true
-    document.body.appendChild(script)
+  const mapContainerRef = useRef(null);
 
-    // Cleanup script on component unmount.
-    return () => {
-      document.body.removeChild(script)
+  useEffect(() => {
+    // Create and append the external script manually in case it isn’t executed from the raw HTML.
+    if (mapContainerRef.current) {
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = 'https://embedmaps.com/google-maps-authorization/script.js?id=327d86f0388a494678b41ab0d16e308d2505298c';
+      script.async = true;
+      mapContainerRef.current.appendChild(script);
     }
-  }, [])
+  }, []);
 
   return (
     <div className="w-screen bg-white/10 min-h-[300px] flex flex-col items-center justify-center pt-8 px-4">
@@ -42,31 +42,21 @@ const BestSpaSection = () => {
         className="h-20 filter brightness-0 sepia-100 hue-rotate-330 saturate-50"
       />
       {/* Responsive Map Embed */}
-      <div className="mt-6 flex flex-col items-center w-full">
-        <div className="w-full max-w-xl">
-          {/* Container maintains the aspect ratio: 520 x 400 ~ 1.3 ratio (77% padding-bottom) */}
-          <div className="relative w-full" style={{ paddingBottom: '77%', height: 0 }}>
-            <iframe 
-              title="Google Map"
-              className="absolute top-0 left-0 w-full h-full"
-              frameBorder="0" 
-              scrolling="no" 
-              marginHeight="0" 
-              marginWidth="0" 
-              id="gmap_canvas" 
-              src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=%20Los%20Angeles+()&amp;t=k&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
-            </iframe>
-          </div>
+      <div className="mt-6 flex flex-col items-center w-full max-w-xl" ref={mapContainerRef}>
+        <div className="relative w-full" style={{ paddingBottom: '77%', height: 0 }}>
+          <div className="absolute top-0 left-0 w-full h-full"
+            dangerouslySetInnerHTML={{
+              __html: `
+                <iframe width="520" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=%20Los%20Angeles+()&amp;t=k&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+                <a href="https://www.easybooking.eu/produkte/julia-hotelsoftware">Hotelprogramm Buchungen</a>
+                <script type="text/javascript" src="https://embedmaps.com/google-maps-authorization/script.js?id=327d86f0388a494678b41ab0d16e308d2505298c"></script>
+              `
+            }}
+          />
         </div>
-        <a 
-          href="https://www.easybooking.eu/produkte/julia-hotelsoftware" 
-          className="mt-2 text-sm underline text-light-brown"
-        >
-          Hotelprogramm Buchungen
-        </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default BestSpaSection
+export default BestSpaSection;
