97e1e0c614716dba1934d9e3daaf932ee4cbc577
sphinx91889 <mevin.riviere@gmail.com>
2025-02-09 18:13:53 -0500

    Update BestSpaSection.jsx

--- src/components/BestSpaSection.jsx
+++ src/components/BestSpaSection.jsx
@@ -1,19 +1,6 @@
-import React, { useEffect, useRef } from 'react'
+import React from 'react';
 
 const BestSpaSection = () => {
-  const mapContainerRef = useRef(null)
-
-  useEffect(() => {
-    // Dynamically load the external script for map authorization.
-    if (mapContainerRef.current) {
-      const script = document.createElement('script')
-      script.type = 'text/javascript'
-      script.src = 'https://embedmaps.com/google-maps-authorization/script.js?id=327d86f0388a494678b41ab0d16e308d2505298c'
-      script.async = true
-      mapContainerRef.current.appendChild(script)
-    }
-  }, [])
-
   return (
     <div className="w-screen bg-white/10 min-h-[300px] flex flex-col items-center justify-center pt-8 px-4 mb-8">
       <h2 className="text-4xl md:text-5xl text-light-brown font-display font-semibold text-center mb-4">
@@ -42,20 +29,23 @@ const BestSpaSection = () => {
         className="h-20 filter brightness-0 sepia-100 hue-rotate-330 saturate-50"
       />
       {/* Responsive Map Embed */}
-      <div className="mt-6 flex flex-col items-center w-full max-w-xl" ref={mapContainerRef}>
-        <div className="relative w-full" style={{ paddingBottom: '77%', height: 0 }}>
-          <div className="absolute top-0 left-0 w-full h-full"
-            dangerouslySetInnerHTML={{
-              __html: `
-                <iframe width="520" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=%20Los%20Angeles+()&amp;t=k&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
-                <script type="text/javascript" src="https://embedmaps.com/google-maps-authorization/script.js?id=327d86f0388a494678b41ab0d16e308d2505298c"></script>
-              `
-            }}
-          />
+      <div className="mt-6 flex flex-col items-center w-full max-w-xl">
+        <div className="relative w-full" style={{ paddingBottom: '75%', height: 0 }}>
+          <iframe 
+            title="Google Map"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3314.7222406566093!2d-118.39148178829592!3d33.8194803298726!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dd4b56c300d84f%3A0x96620e6f02c97947!2sSpade%20Skin%20Care%20%26%20More%20Inc.!5e0!3m2!1sen!2sus!4v1739140193534!5m2!1sen!2sus"
+            width="600"
+            height="450"
+            style={{ border: 0 }}
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            className="absolute top-0 left-0 w-full h-full"
+          ></iframe>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default BestSpaSection
+export default BestSpaSection;
